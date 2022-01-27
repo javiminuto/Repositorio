10ab15d0a94e4951fb7bb907e9039607521b9a71
javiminuto <javisalo1@hotmail.com>
2022-01-27 15:57:01 +0100

    Calculos primos con filter

--- ejercicioa.py
+++ ejercicioa.py
@@ -3,3 +3,23 @@ cols = 4
 
 m = [[i*cols+j for j in range(cols)] for i in range(row)]
 print(m)
+
+def get_maximo(lst):
+    return max(lst)
+
+elem_maximos = list(map(get_maximo, m))
+
+print("Los elementos mayores son :",elem_maximos)
+
+def es_primo(n):
+    primo=True
+    for i in range(2,n):
+        if n%i==0:
+            primo=False
+    return primo
+
+#num_primos = list(map(es_primo, elem_maximos))
+
+num_primos = list(filter(es_primo, elem_maximos))
+
+print("Los primos son :",num_primos)
