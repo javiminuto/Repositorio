c0c53ca98552eea260233fadd7f52d985c2c122b
javiminuto <javisalo1@hotmail.com>
2022-01-27 15:49:24 +0100

    Compresion listas init matriz

--- ejercicioa.py
+++ ejercicioa.py
@@ -1,10 +1,5 @@
 row = 8
 cols = 4
 
-m = []
-for i in range(row):
-    r = []
-    for j in range(cols):
-        r.append(i*cols+j)
-    m.append(r)
+m = [[i*cols+j for j in range(cols)] for i in range(row)]
 print(m)
